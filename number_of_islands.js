51a8a38003e93dcf6e066f84eae6f10511eb6389
Plotnikov, Anton <anton.plotnikov@gmail.com>
2017-08-12 00:51:55 +0100

    number of islands / leetcode / problem 200

--- number_of_islands.js
+++ number_of_islands.js
@@ -1,6 +1,14 @@
-// https://leetcode.com/problems/number-of-islands/description/ 
-// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
-// An island is surrounded by water and is formed by connecting adjacent lands 
+// https://leetcode.com/problems/number-of-islands/description/
+// Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+
+// 11000
+// 11000
+// 00100
+// 00011
+
+// Answer: 3
+
+// An island is surrounded by water and is formed by connecting adjacent lands
 // horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
 var toGrid = function(input) {
@@ -17,35 +25,35 @@ var numIslands = function(grid) {
             return false;
         }));
     }
-    
+
     var islandCounter = 0;
     var walk = function(x,y) {
         seen[x][y] = true;
-        
+
         var move = {
             'up':    function(x,y) { return [x,y-1]; },
             'down':  function(x,y) { return [x,y+1]; },
             'left':  function(x,y) { return [x-1,y]; },
             'right': function(x,y) { return [x+1,y]; }
         }
-        
+
         var ways = [move.up,move.down, move.left, move.right];
         for(var i=0; i<ways.length; i++) {
             var m = ways[i];
             var point = m(x,y);
             var px = point[0];
             var py = point[1];
-            if (typeof seen[px] !== 'undefined' && 
-                !seen[px][py] &&  
-                typeof grid[px] !== 'undefined' && 
-                grid[px][py] === '1') 
+            if (typeof seen[px] !== 'undefined' &&
+                !seen[px][py] &&
+                typeof grid[px] !== 'undefined' &&
+                grid[px][py] === '1')
             {
                 walk(px, py);
             }
         }
         return;
     };
-    
+
     var isVisited = function(x,y) {
         return seen[x][y];
     };
