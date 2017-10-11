c85674710953e0574c32912355ffe706ce9649b8
Plotnikov, Anton <anton.plotnikov@gmail.com>
2017-10-11 10:24:40 +0100

    minimum Window substring / leetcode / 76

--- min_window_sustring.js
+++ min_window_sustring.js
@@ -1,17 +1,21 @@
 var minWindow = function(s, t) {
+    // create hashmap for letters => amount
+    // e.g. { a: 2, b: 1 }
     var need = t.split('').reduce(function(prev, item) {
-        prev[item] = 1;
+        prev[item] = (typeof prev[item] == 'undefined' ? 1 : prev[item]+1);
         return prev;
     }, {});
+
     var missing = t.length;
     var i = I = J = 0;
-    
+
     for(var x=0; x<s.length; x++) {
         var j = x + 1;
         var c = s[x];
+
         need[c] = (typeof need[c] == 'undefined' ) ? -1 : (need[c]-1);
-        missing -= (need[c] == 0 ? 1: 0);
-        
+        missing -= (need[c] < 0 ? 0: 1);
+
         if (missing <= 0) {
             while ( i<j && need[s[i]] < 0) {
                 need[s[i]] += 1;
@@ -26,5 +30,5 @@ var minWindow = function(s, t) {
     return s.slice(I,J);
 };
 
-var result = minWindow('AXXBCXXXXBAC', 'ABC');
-console.log(result)
+console.log(minWindow('AXXBCXXXXBAAC', 'ABCA'));
+console.log(minWindow('aa', 'aa'));
