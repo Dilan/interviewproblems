6dc066e71a4718597f3dbb7845f23e0cddf9e6ae
Plotnikov, Anton <anton.plotnikov@gmail.com>
2019-02-17 15:41:56 +0000

    subset

--- lib/subsets.py
+++ lib/subsets.py
@@ -1,11 +1,34 @@
-# all available subsets
-def subset(list):
-    if len(list) == 1:
-        return [list]
+class Solution(object):
+    def subsets(self, nums):
+        result = [[]]
+        for num in nums:
+            for arr in result[:]:
+                result.append([num]+arr)
+        return result
 
-    result = [[list[0]]]
-    for s in subset(list[1:]):
-        result.append(s)
-        result.append([list[0]] + s)
+    def subset_recursion(self, list):
+        if len(list) == 1:
+            return [list]
 
-    return result
+        result = [[list[0]]]
+        for s in self.subset_recursion(list[1:]):
+            result.append(s)
+            result.append([list[0]] + s)
+
+        return result
+
+if  __name__ == '__main__':
+    data = [
+        (
+            [1,2,3],
+            [[3],[1],[2],[1,2,3],[1,3],[2,3],[1,2],[]]
+        ),
+
+    ]
+
+    for item in data:
+        nums, answer = item
+        result = Solution().subset_recursion(nums)
+        print(nums)
+        print('Result:')
+        print(result)
