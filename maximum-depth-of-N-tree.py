8fe2b52e66e5cca5e2c39e82bd567dcb9eaafa17
Plotnikov, Anton <anton.plotnikov@gmail.com>
2019-11-20 22:00:05 +0000

    Maximum Depth of N-ary Tree / leetcode / problem 559

---
+++ maximum-depth-of-N-tree.py
@@ -0,0 +1,54 @@
+# https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution(object):
+    @staticmethod
+    def dfs_recursion(root, depth=1):
+        max_depth = depth
+        if root.children is not None and len(root.children) > 0:
+            for n in root.children:
+                val = Solution.dfs(n, depth + 1)
+                max_depth = max(val,max_depth)
+
+        return max_depth
+
+    @staticmethod
+    def dfs(root):
+        max_depth = 0
+        stack = [(root, 1)]
+        while len(stack):
+            node, depth = stack.pop(0)
+            max_depth = max(depth,max_depth)
+            if node.children is not None and len(node.children) > 0:
+                for n in node.children:
+                    stack.extend([(n, depth + 1)])
+        return max_depth
+
+    @staticmethod
+    def bfs(root):
+        max_depth = 0
+        stack = [(root, 1)]
+        while len(stack):
+            node, depth = stack.pop(0)
+            max_depth = max(depth,max_depth)
+            if node.children is not None and len(node.children) > 0:
+                for n in node.children:
+                    stack.extend([(n, depth + 1)])
+        return max_depth
+
+    def maxDepth(self, root):
+        if root is None: return 0
+
+        max_depth = Solution.dfs(root)
+        return max_depth
+
+
+if __name__ == '__main__':
+    root = Node(1, [Node(2),Node(3), Node(4)])
+
+    answer = Solution().maxDepth(root)
+    print(answer)
